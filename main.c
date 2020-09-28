be7d8d9ab8c391163f490c4381b493b3f8a765f6
Felipe Ribeiro <felipelrib@gmail.com>
2020-09-28 05:42:00 -0300

    Use another constant for a lower amount of threads

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 #include <sys/time.h>
 #include "binaryTree.h"
 
-#define NUM_THREADS 4
+#define NUM_THREADS 8
 
 /**
  * @brief Argumentos usados para chamadas dos métodos das diferentes fases
@@ -40,34 +40,51 @@ void permut(RegistryType A[], int n) {
 
 void *insertionPhase(void *phaseArgs) {
   PhaseArgs *insertionPhaseArgs = (PhaseArgs *) phaseArgs;
-  insert(insertionPhaseArgs->vetor[insertionPhaseArgs->threadIndex], insertionPhaseArgs->root);
+  int i;
+  int threadIndex = insertionPhaseArgs->threadIndex;
+  int startValue = (threadIndex * MAX / NUM_THREADS);
+  int endValue = ((threadIndex + 1) * MAX / NUM_THREADS);
+  for (i = startValue; i < endValue; i++) {
+    insert(insertionPhaseArgs->vetor[i], insertionPhaseArgs->root);
+  }
   barreira(insertionPhaseArgs->barrier);
 }
 
 void *removeSearchInsertPhase(void *phaseArgs) {
   PhaseArgs *removeSearchInsertPhaseArgs = (PhaseArgs *) phaseArgs;
+  int threadIndex = removeSearchInsertPhaseArgs->threadIndex;
+  int startValue = (threadIndex * MAX / NUM_THREADS);
+  int dataPartitionAmount = MAX / NUM_THREADS;
   int i, j, k, n;
   RegistryType aux;
   RegistryType *vetor = removeSearchInsertPhaseArgs->vetor;
-  k = (int)(10.0 * rand() / (RAND_MAX + 1.0));
-  aux = vetor[k];
-  removeValue(vetor[k], removeSearchInsertPhaseArgs->root);
-  printf("Retirou chave: %ld\n", vetor[k].key);
-  for (j = 0; j < MAX; j++) {
-    k = (int)(10.0 * rand() / (RAND_MAX + 1.0));
-    if (vetor[k].key != aux.key) {
-      printf("Pesquisando chave: %ld\n", vetor[k].key);
-      search(&vetor[k], removeSearchInsertPhaseArgs->root);
+  for (i = 0; i < dataPartitionAmount; i++) {
+    k = (int)((float) dataPartitionAmount * rand() / (RAND_MAX + 1.0)) + startValue;
+    aux = vetor[k];
+    removeValue(vetor[k], removeSearchInsertPhaseArgs->root);
+    printf("Retirou chave: %ld\n", vetor[k].key);
+    for (j = 0; j < dataPartitionAmount; j++) {
+      k = (int)((float) dataPartitionAmount * rand() / (RAND_MAX + 1.0)) + startValue;
+      if (vetor[k].key != aux.key) {
+        printf("Pesquisando chave: %ld\n", vetor[k].key);
+        search(&vetor[k], removeSearchInsertPhaseArgs->root);
+      }
     }
+    insert(aux, removeSearchInsertPhaseArgs->root);
+    printf("Inseriu chave: %ld\n", aux.key);
   }
-  insert(aux, removeSearchInsertPhaseArgs->root);
-  printf("Inseriu chave: %ld\n", aux.key);
   barreira(removeSearchInsertPhaseArgs->barrier);
 }
 
 void *removalPhase(void *phaseArgs) {
   PhaseArgs *removalPhaseArgs = (PhaseArgs *) phaseArgs;
-  removeValue(removalPhaseArgs->vetor[removalPhaseArgs->threadIndex], removalPhaseArgs->root);
+  int i;
+  int threadIndex = removalPhaseArgs->threadIndex;
+  int startValue = (threadIndex * MAX / NUM_THREADS);
+  int endValue = ((threadIndex + 1) * MAX / NUM_THREADS);
+  for (i = startValue; i < endValue; i++) {
+    removeValue(removalPhaseArgs->vetor[i], removalPhaseArgs->root);
+  }
   barreira(removalPhaseArgs->barrier);
 }
 
@@ -79,9 +96,9 @@ int main(int argc, char *argv[]) {
   int i;
 
   // TODO Não deve ter essa quantidade de threads por conta do overhead.
-  pthread_t threads[MAX];
+  pthread_t threads[NUM_THREADS];
 
-  initBarreira(&barrier, MAX + 1);
+  initBarreira(&barrier, NUM_THREADS + 1);
   initRoot(&root);
   /* Gera uma permutação aleatoria de chaves entre 1 e MAX */
   for (i = 0; i < MAX; i++)
@@ -91,8 +108,8 @@ int main(int argc, char *argv[]) {
   permut(vetor, MAX - 1);
 
   /* Insere cada chave na arvore e testa sua integridade apos cada insercao */
-  PhaseArgs phaseArgs[MAX];
-  for (i = 0; i < MAX; i++) {
+  PhaseArgs phaseArgs[NUM_THREADS];
+  for (i = 0; i < NUM_THREADS; i++) {
     phaseArgs[i].vetor = vetor;
     phaseArgs[i].barrier = &barrier;
     phaseArgs[i].root = &root;
@@ -103,14 +120,14 @@ int main(int argc, char *argv[]) {
   test(root);
 
   /* Retira uma chave aleatoriamente e realiza varias pesquisas */
-  // for (i = 0; i < MAX; i++) {
-  //   pthread_create(&(threads[i]), NULL, removeSearchInsertPhase, &(phaseArgs[i]));
-  // }
-  // barreira(&barrier);
-  // test(root);
+  for (i = 0; i < NUM_THREADS; i++) {
+    pthread_create(&(threads[i]), NULL, removeSearchInsertPhase, &(phaseArgs[i]));
+  }
+  barreira(&barrier);
+  test(root);
 
   /* Retira a raiz da arvore ate que ela fique vazia */
-  for (i = 0; i < MAX; i++) {
+  for (i = 0; i < NUM_THREADS; i++) {
     pthread_create(&(threads[i]), NULL, removalPhase, &(phaseArgs[i]));
   }
   barreira(&barrier);
